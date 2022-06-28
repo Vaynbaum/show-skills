8c990eadc53232e47c62e2a8c8aa1ff895db64db
Денис <mr.vaynbaum@mail.ru>
2022-06-28 12:14:33 +0300

    небольшие изменения в main.py

--- backend/main.py
+++ backend/main.py
@@ -1,9 +1,11 @@
 import os
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi import FastAPI
+from dotenv import load_dotenv
 
 from routes import auth_router, user_router
 
+load_dotenv()
 app = FastAPI(title=os.getenv("PROJECT_NAME"))
 
 app.add_middleware(
